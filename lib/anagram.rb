c86fde0473bd936a2a3eaa6b0a1657e470a38833
jim <jbmbroomfield@gmail.com>
2021-07-14 22:20:46 +1000

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class String
+    def sort
+        self.chars.sort.join
+    end
+end
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        self.word = word
+    end
+
+    def match(array)
+        array.filter do |other_word|
+            other_word.sort == word.sort
+        end
+    end
+end
